6bb36d1802943320985a68305a1d5e4dd5af117b
macmillianzungene <macmillianzungene@gmail.com>
2023-07-14 19:10:01 -0700

    comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+/**
+  * main - Entry point
+  * Description: 'print different combination of two digits'
+  * Return: Always 0
+  */
+int main(void)
+{
+	int n = 0;
+	int m, w;
+
+	while (n <= 9)
+	{
+		m = 0;
+		while (m <= 9)
+		{
+			w = 0;
+			while (w <= 9)
+			{
+				if (n != m &&
+				    n < m &&
+				    m != w &&
+				    m < w)
+				{
+					putchar(n + 48);
+					putchar(m + 48);
+					putchar(w + 48);
+
+					if (n + m + w != 24)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+				w++;
+			}
+			m++;
+		}
+		n++;
+	}
+	putchar('\n');
+
+	return (0);
+}
