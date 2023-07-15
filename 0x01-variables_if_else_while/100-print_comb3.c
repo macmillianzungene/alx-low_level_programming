d5e676e52bab491c950697d1915f815f48b9c62a
macmillianzungene <macmillianzungene@gmail.com>
2023-07-14 18:10:23 -0700

    combos

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+  * main - Entry point
+  * Description: 'print different combination of two digits'
+  * Return: Always 0
+  */
+int main(void)
+{
+	int n;
+	int m;
+
+	for (n = 0; n < 10; n++)
+	{
+		for (m = n + 1; m < 10; m++)
+		{
+
+		putchar((n % 10) + '0');
+		putchar((m % 10) + '0');
+
+		if (n == 8 && m == 9)
+			continue;
+		putchar(',');
+		putchar(' ');
+		}
+	}
+
+	return (0);
+}
