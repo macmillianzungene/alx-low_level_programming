bc8bb3a40c4d9c287e45f405991bcf97635ae30c
macmillianzungene <macmillianzungene@gmail.com>
2023-07-19 07:26:09 -0700

    fib 103

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,14 +1,14 @@
 #include "main.h"
 /**
   * main - Entry point
-  *i
-  * Description: print the sum of the even numbers.
+  *
+  * Description: print the sum of the even-valued fibonacci numbers.
   *
   * return: Always 0 (Success)
   */
 int main(void)
 {
-	unsigned long int fib1 = 0, fib2 = 1, sum;
+	unsigned long fib1 = 0, fib2 = 1, sum;
 	float total_sum;
 
 	while (1)
@@ -17,13 +17,13 @@ int main(void)
 
 		if (sum > 4000000)
 			break;
+
 		if ((sum % 2) == 0)
 			total_sum += sum;
 
 		fib1 = fib2;
 		fib2 = sum;
 	}
-	printf("%lu\n", sum);
-
+	printf("%lu\n", total_sum);
 	return (0);
 }
