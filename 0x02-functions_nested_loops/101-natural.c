6aa910819875ca3cedc9ee51355efde349271b5e
macmillianzungene <macmillianzungene@gmail.com>
2023-07-18 19:46:18 -0700

    natural

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,25 @@
+/*
+ * File: 101-natural.c
+ * Auth: vincent kip
+ */
+
+#include <stdio.h>
+/**
+  * main - list natural numbers below 1024 excluding that multiplies 3 or 5.
+  *
+  * Return: Always 0
+  */
+int main(void)
+{
+	int i, sum = 0;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if ((i % 3) == 0 || (i % 5) == 0)
+			sum += i;
+	}
+
+	printf("%d\n", sum);
+
+	return (0);
+}
