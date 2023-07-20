ec511afe9eda346e882513176ebdfd4c0f510f0a
macmillianzungene <macmillianzungene@gmail.com>
2023-07-20 16:55:13 -0700

    mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+  * print_numbers - print 0 - 9 numbers
+  *
+  * Return: Always 0
+  */
+void print_numbers(void)
+{
+	int n = 0;
+
+	do {
+		_putchar(n + 48);
+		n++;
+	} while (n >= 0 && n <= 9);
+	_putchar('\n');
+}
