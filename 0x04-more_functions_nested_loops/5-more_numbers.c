dc8cf6e28af09fe37f9a43d9294abe3c9d7dd02b
macmillianzungene <macmillianzungene@gmail.com>
2023-07-20 17:45:50 -0700

    more numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+  * more_numbers - print 10 times numbers 0 to 14
+  *
+  *
+  * Return: Always 0
+  */
+void more_numbers(void)
+{
+	int n, r, c;
+
+	for (r = 1; r <= 10; r++)
+	{
+		for (c = 0; c <= 14; c++)
+		{
+			n = c;
+			if (c > 9)
+			{
+				_putchar(1 + 48);
+				n = c % 10;
+			}
+			_putchar(n + 48);
+		}
+		_putchar('\n');
+	}
+}
