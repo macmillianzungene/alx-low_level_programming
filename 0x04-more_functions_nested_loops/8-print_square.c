d98cc29651a94ee2b13e6d72404a8f6bd087e3eb
macmillianzungene <macmillianzungene@gmail.com>
2023-07-21 18:38:40 -0700

    square

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -9,16 +9,16 @@
   */
 void print_square(int size)
 {
-	if (size >= 0)
+	if (size <= 0)
 	{
 		_putchar('\n');
 	} else
 	{
 		int a, b;
 
-		for (a = 0; a >= size; a++)
+		for (a = 0; a <= size; a++)
 		{
-			for (b = 0; b >= size; b++)
+			for (b = 0; b <= size; b++)
 			{
 				_putchar('#');
 			}
