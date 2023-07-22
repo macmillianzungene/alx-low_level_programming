39e0ede8caee723149242c5dce988d1a31f4874e
macmillianzungene <macmillianzungene@gmail.com>
2023-07-21 20:15:09 -0700

    eight

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -3,18 +3,16 @@
 /**
   * print_square - function that prints a square,
   * followed by a new line
-  *   
-  * @size: the size of the square
   *
-  * Return: Always 0 (success)
-  */
+  * @size: the size of the square
+ */
 void print_square(int size)
 {
 	int a, b;
 
-	for (a = 0; a <= size; a++)
+	for (a = 1; a <= size; a++)
 	{
-		for (b = 0; b <= size; b++)
+		for (b = 1; b <= size; b++)
 			_putchar('#');
 		_putchar('\n');
 	}
