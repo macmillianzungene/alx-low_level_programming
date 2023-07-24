1ee1ffc5d2411c1243fd49374244d2ffa388758e
macmillianzungene <macmillianzungene@gmail.com>
2023-07-24 14:55:29 -0700

    crackme

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+  * main - generates random valid passwords for 101-crackme.
+  *
+  * Return: 0
+  */
+int main(void)
+{
+	int psd;
+	char c;
+
+	srand(time(NULL));
+	while (psd <= 2654)
+	{
+		c = rand() % 128;
+		psd += c;
+		putchar(c);
+	}
+	putchar(2772 - psd);
+	return (0);
+}
