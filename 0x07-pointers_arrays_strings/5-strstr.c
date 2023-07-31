5155f7fa67bb5c9b6fbbc5b2c99bdddee18a2760
macmillianzungene <macmillianzungene@gmail.com>
2023-07-31 15:20:16 -0700

    strstr

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -10,4 +10,26 @@
   */
 char *_strstr(char *haystack, char *needle)
 {
+	int b;
 
+	if (*needle == 0)
+		return (haystack);
+
+	while (*haystack)
+	{
+		b = 0;
+
+		if (haystack[b] == needle[b])
+		{
+			do {
+				if (needle[b + 1] == '\0')
+					return (haystack);
+				b++;
+			} while (haystack[b] == needle[b]);
+		}
+
+		haystack++;
+	}
+
+	return ('\0');
+}
