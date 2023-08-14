066445f349d4977ceee3206f195e93e5aaceadf9
macmillianzungene <macmillianzungene@gmail.com>
2023-08-14 15:29:48 -0700

    structures

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+  * struct dog - dog struct
+  *
+  * @name: name of the dog
+  * @age: age of the dog
+  * @owner: owner of the dog
+  *
+  * Description: dog structure
+  */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
