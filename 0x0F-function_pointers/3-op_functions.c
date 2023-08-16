15cf550d1673322b4eede2b2f58dcb13666c2915
macmillianzungene <macmillianzungene@gmail.com>
2023-08-16 16:43:30 -0700

    operations

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,66 @@
+#include "3-calc.c"
+
+/**
+  * op_add - adds two integers
+  *
+  * @a: first input
+  * @b: second input
+  *
+  * Return: results
+  */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+  * op_sub - subtracts two integers
+  *
+  * @a: first input
+  * @b: second input
+  *
+  * Return: results
+  */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+  * op_mul - multiplies two integers
+  *
+  * @a: first input
+  * @b: second input
+  *
+  * Return: results
+  */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+  * op_div - divides two intengers
+  *
+  * @a: first input
+  * @b: second input
+  *
+  * Return: results
+  */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+  * op_mod - remainder of the division of two intenger
+  *
+  * @a: first input
+  * @b: second input
+  *
+  * Return: results
+  */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
