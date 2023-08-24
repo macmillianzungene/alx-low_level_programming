eedd8440935c26d6c88263b5b28e8d4c2213bcbf
macmillianzungene <macmillianzungene@gmail.com>
2023-08-24 14:44:47 -0700

    printing lists

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,39 @@
+#include "lists.h"
+
+/**
+  * _strlen - prints the length of a string
+  *
+  * @s: input string
+  *
+  * Return: results
+  */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	if (!s)
+		return (0);
+	while (*s++)
+		i++;
+	return (i);
+}
+
+/**
+  * print_list - function that prints all the elements of a list_t list.
+  *
+  * @h: input pointer
+  *
+  * Return: results
+  */
+size_t print_list(const list_t *h)
+{
+	size_t i = 0;
+
+	while (h)
+	{
+		printf("[%d] %s\n", _strlen(h->str), h->str ? h->str : "(nil)");
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
