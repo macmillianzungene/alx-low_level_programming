e8135dc9fec94892271d0ede91568a8245991243
macmillianzungene <macmillianzungene@gmail.com>
2023-08-25 16:54:36 -0700

    Hello Holberton

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -2,9 +2,9 @@ global   main
         extern printf
 main:
         mov     edi, format
-	mov     eax. 0
-	call    printf
 	xor     eax, eax
+	call    printf
+	mov     eax, 0
 	ret
-format: db 'Hello, Holberton\n', 0
+format: db `Hello, Holberton\n`,0
 
