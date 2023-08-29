0957d5e020626973166794a76e049f9821a4373e
macmillianzungene <macmillianzungene@gmail.com>
2023-08-29 16:41:17 -0700

    freeing listint safe

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,4 +1,31 @@
 #include "lists.h"
+#include <stdlib.h>
+
+/**
+  * find_listint_loop_fl - locating the loop in a list
+  *
+  * @head: input pointer
+  *
+  * Return: results
+  */
+listint_t *find_listint_loop_fl(listint_t *head)
+{
+	listint *ptr, *end;
+
+	if (head == NULL)
+		return (NULL);
+
+	for (end = head->next; end != NULL; end = end->next)
+	{
+		if (end == end->next)
+			return (end);
+		for (ptr = head; ptr != end; ptr = ptr->next)
+			if (ptr == end->next)
+				return (end->next);
+	}
+	return (NULL);
+}
+
 
 /**
   * free_listint_safe - function that frees a listint_t list.
@@ -9,3 +36,32 @@
   */
 size_t free_listint_safe(listint_t **h)
 {
+	listint_t *next, *node;
+	size_t len;
+	int rnd = 1;
+
+	if (h == NULL || *h == NULL)
+		return (0);
+
+	node = find_listint_loop_fl(*h);
+	for (len = 0; (*h != node || rnd) && *h != NULL; *h = next)
+	{
+		len++;
+		next = (*h)->next;
+		if (*h == node && rnd)
+		{
+			if (node == node->next)
+			{
+				free(*h);
+				break;
+			}
+			len++;
+			next = next->next;
+			free((*h)->next);
+			rnd = 0;
+		}
+		free(*h);
+	}
+	*h = NULL;
+	return (len);
+}
---
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+
+/**
+  * find_listint_loop - function that finds the loop in a linked list.
+  *
+  * @head: input pointer
+  *
+  * Return: results
+  */
+listint_t *find_listint_loop(listint_t *head)
+{
