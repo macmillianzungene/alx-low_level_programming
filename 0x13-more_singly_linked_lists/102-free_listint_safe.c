3c7775fe2415ba0b3bb1519e201c7dc5e00c9b93
macmillianzungene <macmillianzungene@gmail.com>
2023-08-29 16:00:53 -0700

    printing listint safe

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+
+/**
+  * free_listint_safe - function that frees a listint_t list.
+  *
+  * @h: input value
+  *
+  * Return: results
+  */
+size_t free_listint_safe(listint_t **h)
+{
