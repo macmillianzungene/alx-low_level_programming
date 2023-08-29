991dc1f7a1835f9d6388843224e1fdeeeeb7f760
macmillianzungene <macmillianzungene@gmail.com>
2023-08-29 14:14:29 -0700

    poping listint

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -9,3 +9,15 @@
   */
 int pop_listint(listint_t **head)
 {
+	listint_t *node;
+	int n;
+
+	if (!head || !*head)
+		return (0);
+
+	node = (*head)->next;
+	n = (*head)->n;
+	free(*head);
+	*head = node;
+	return (n);
+}
