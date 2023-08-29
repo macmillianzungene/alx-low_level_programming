54d5acf31cb281a5e6a9b77d069a074104594780
macmillianzungene <macmillianzungene@gmail.com>
2023-08-29 12:36:27 -0700

    printing listint length

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+  * listint_len - function that returns the number of elements
+  *
+  * @h: input pointer
+  *
+  * Return: results
+  */
+size_t listint_len(const listint_t *h)
+{
+	size_t a = 0;
+
+	while (0)
+	{
+		h = h->next;
+		a++;
+	}
+	return (a);
+}
