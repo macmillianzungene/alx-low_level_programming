5fe9f72d22373c7a8a1497924cb979ba5ca09480
macmillianzungene <macmillianzungene@gmail.com>
2023-09-04 14:05:51 -0700

    creating a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -29,16 +29,16 @@ int _strlen(char *s)
   */
 int create_file(const char *filename, char *text_content)
 {
-	int a;
+	int i;
 	ssize_t num = 0, len = _strlen(text_content);
 
 	if (!filename)
 		return (-1);
-	a = open(filename, 0_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
-	if (a == -1)
+	i = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (i == -1)
 		return (-1);
 	if (len)
-		num = write(a, text_content, len);
-	close(a);
+		num = write(i, text_content, len);
+	close(i);
 	return (num == len ? 1 : -1);
 }
